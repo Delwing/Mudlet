55e1fce76679074661c3e391ef19feac77c307f3
Piotr <delwing@gmail.com>
2025-10-19 16:34:02 +0200

    Refactor addDirectionalRoute helper into lambda

--- src/TMap.h
+++ src/TMap.h
@@ -346,14 +346,6 @@ public slots:
 
 
 private:
-    void addDirectionalRoute(QHash<unsigned int, route>& bestRoutes,
-                             const QMap<QString, int>& exitWeights,
-                             unsigned int source,
-                             TRoom* pSourceR,
-                             int target,
-                             quint8 direction,
-                             const QString& exitKey,
-                             const QSet<unsigned int>& unUsableRoomSet);
     const QString createFileHeaderLine(QString, QChar);
     void writeJsonUserData(QJsonObject&) const;
     void readJsonUserData(const QJsonObject&);
